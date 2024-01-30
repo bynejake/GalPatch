5476d69b322a5be3d0f32c341bdce2684300049a
bynejake <bynejake007@gmail.com>
2024-01-31 00:01:55 +0800

    v1.0

---
+++ WafflePatch/dllmain.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        Hijacker::Hijack(L"Waffle");
+        LoadLibHooker::Hook();
+        WaffleInjector::Inject(
+            []
+            {
+                WafflePatcher::Patch();
+            });
+        break;
+    case DLL_PROCESS_DETACH:
+        WafflePatcher::Unpatch();
+        WaffleInjector::Eject();
+        LoadLibHooker::Unhook();
+        Hijacker::Release();
+        break;
+    default:
+        break;
+    }
+
+    return TRUE;
+}
---
+++ WafflePatch/pch.h
@@ -0,0 +1,12 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "framework.h"
+#include "Hijacker.h"
+#include "DetoursHelper.h"
+#include "LoadLibHooker.h"
+
+#include "WaffleInjector.h"
+#include "WafflePatcher.h"
+
+#endif //PCH_H
