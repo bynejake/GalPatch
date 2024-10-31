ec5e75c5aa4a3af1e6d98c55c508a66677900ca6
bynejake <bynejake007@gmail.com>
2024-10-31 22:18:06 +0800

    add protected exe encode patch, only applicable to game : ナイショの姦淫～真夏の汗だく交尾～ for now

--- WafflePatch/WafflePatcher.cpp
+++ WafflePatch/WafflePatcher.cpp
@@ -2,12 +2,43 @@
 
 void WafflePatcher::Patch()
 {
+    //PatchProtectedExeEncode();
+
     if (OriginalGetTextExtentPoint32A != nullptr)
         return;
 
     OriginalGetTextExtentPoint32A = GetTextExtentPoint32A;
     DetoursHelper::Hook(std::pair(&OriginalGetTextExtentPoint32A, PatchGetTextExtentPoint32A));
 }
+
+// Now only applicable to game : ¥Ê¥¤¥·¥ç¤ÎŠ¦Òù¡«ÕæÏÄ¤Îº¹¤À¤¯½»Î²¡« 
+//void WafflePatcher::PatchProtectedExeEncode()
+//{
+//    struct MemoryPatch
+//    {
+//        DWORD* addr;
+//        DWORD               check;
+//        std::vector<BYTE>   patch;
+//    };
+//
+//    static const MemoryPatch MemoryPatches[] = {
+//        // encoding
+//        {reinterpret_cast<DWORD*>(0x004E6D24), 0x00008068, {0x68, 0x86}},
+//        // boundary
+//        {reinterpret_cast<DWORD*>(0x0046BB6A), 0x06769F3C, {0x3C, 0xFE}},
+//        // font
+//        {
+//            reinterpret_cast<DWORD*>(0x0068EE34), 0X72826c82,
+//            {0x55, 0x6E, 0x69, 0x66, 0x6F, 0x6E, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+//        }
+//    };
+//
+//    for (const auto& [addr, check, patch] : MemoryPatches)
+//    {
+//        if (*addr == check)
+//            std::memcpy(addr, patch.data(), patch.size());
+//    }
+//}
 
 void WafflePatcher::Unpatch()
 {
--- WafflePatch/WafflePatcher.h
+++ WafflePatch/WafflePatcher.h
@@ -8,6 +8,7 @@ public:
 
 private:
     static BOOL APIENTRY    PatchGetTextExtentPoint32A  (HDC hdc, LPCSTR lpString, int c, LPSIZE lpsz);
+    //static void             PatchProtectedExeEncode();
 
     static inline decltype(GetTextExtentPoint32A)* OriginalGetTextExtentPoint32A;
 };
