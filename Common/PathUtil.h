19cdcc74aedabe0c9b3532a325b7ff0bb29c1168
bynejake <bynejake007@gmail.com>
2024-11-04 16:48:06 +0800

    refactor PathUtil

--- Common/PathUtil.h
+++ Common/PathUtil.h
@@ -1,16 +1,27 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
 #include <string>
+#include <tchar.h>
 
 class PathUtil final
 {
 public:
-    static std::string  GetAppPathA();
-    static std::wstring GetAppPathW();
+    template <typename T>
+    static std::enable_if_t<std::is_same_v<T, std::wstring> || std::is_same_v<T, std::string>, T> GetAppPathTpl()
+    {
+        TCHAR pathBuffer[MAX_PATH];
+        GetModuleFileName(nullptr, pathBuffer, MAX_PATH);
+        T pathStr(pathBuffer);
+
+        return pathStr.substr(0, pathStr.find_last_of(_T('\\')) + 1);
+    }
 
 #ifdef UNICODE
-#define GetAppPath GetAppPathW
+#define GetAppPath GetAppPathTpl<std::wstring>
 #else
-#define GetAppPath GetAppPathA
+#define GetAppPath GetAppPathTpl<std::string>
 #endif
 };
