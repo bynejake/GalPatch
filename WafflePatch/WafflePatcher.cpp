ef8921aca52f01d88f6ae07f6eb7274ef135bb73
bynejake <bynejake007@gmail.com>
2024-12-09 23:33:14 +0800

    waffle patch: read memory patch from version.json file.

--- WafflePatch/WafflePatcher.cpp
+++ WafflePatch/WafflePatcher.cpp
@@ -1,30 +1,48 @@
 #include "pch.h"
 
-// Now only applicable to game : ナイショの姦淫～真夏の汗だく交尾～
+//#include <fstream>
+//
+//#include "nlohmann/json.hpp"
+//
 //void WafflePatcher::PatchProtectedExeEncode()
 //{
 //    struct MemoryPatch
 //    {
+//        std::string         type;
 //        DWORD               rva;
 //        DWORD               check;
 //        std::vector<BYTE>   val;
 //    };
 //
-//    static std::vector<MemoryPatch> patches =
+//    static std::vector<MemoryPatch> sPatches = []
 //    {
-//        // encoding
-//        {0x000D6D24, 0x00008068, {0x68, 0x86}},
-//        // boundary
-//        {0x0005BB6A, 0x06769F3C, {0x3C, 0xFE}},
-//        // font
+//        std::vector<MemoryPatch> patches;
+//        if (std::ifstream jFile(PathUtil::GetAppPath() + L"version.json"); jFile.is_open())
 //        {
-//            0x0027EE34, 0X72826c82,
-//            {0x55, 0x6E, 0x69, 0x66, 0x6F, 0x6E, 0x74, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+//#define Str2Hex(str) (std::stoul(str, nullptr, 16))
+//            for (const auto jConfig = nlohmann::json::parse(jFile); const auto& jPatch : jConfig["MemoryPatch"])
+//            {
+//                MemoryPatch patch
+//                {
+//                    .type   =           jPatch["type"]. get<std::string>(),
+//                    .rva    = Str2Hex(  jPatch["rva"].  get<std::string>()),
+//                    .check  = Str2Hex(  jPatch["check"].get<std::string>())
+//                };
+//
+//                for (const auto& jValElem : jPatch["val"])
+//                {
+//                    patch.val.emplace_back(static_cast<BYTE>(Str2Hex(jValElem.get<std::string>())));
+//                }
+//
+//                patches.emplace_back(patch);
+//            }
+//#undef Str2Hex
 //        }
-//    };
+//        return patches;
+//    }();
 //
 //    static const auto ImageBase = Pe::GetOptionalHeader()->ImageBase;
-//    std::erase_if(patches, [](const MemoryPatch& patch)
+//    std::erase_if(sPatches, [](const MemoryPatch& patch)
 //    {
 //        if (const auto addr = reinterpret_cast<DWORD*>(ImageBase + patch.rva); *addr == patch.check)
 //        {
